087aed555842f6dec7c088ca3d9df221f70126e7
Melissa Spalione <mspalione@strategicmarketing.com>
2020-10-29 17:01:32 -0400

    added world map with covid data. Sterilized data to implement map.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //input required api key for code to work. See Readme for instructions on obtaining api key.
-
+//import countryIds from 'countryIds.js'
 const host =  'covid-193.p.rapidapi.com' 
 const statisticsUrl = `https://${host}/statistics`
 const countriesUrl = `https://${host}/countries`
@@ -16,44 +16,6 @@ const error = document.getElementById('error')
 const dropDown = document.getElementById('dropDown')
 let chart
 
-
-
-let countryStats = () => {
-    const selectedCountry = countries.value 
-    
-    getJson(`${statisticsUrl}?country=${selectedCountry}`, apiheaders)
-        .then(data => {
-            const countryData = data.response[0]
-
-            country = {
-                continent: countryData.continent || 'unknown',
-                continent: countryData.continent || 'unknown',
-                country: countryData.country || 'unknown',
-                day: countryData.day || 'unknown',
-                deaths: countryData.deaths.total || 'unknown',
-                population: countryData.population || 'unknown',
-                tests: countryData.tests.total || 'unknown',
-                newCases: countryData.cases.new || 'unknown',
-                activeCases: countryData.cases.active || 'unknown',
-                criticalCases: countryData.cases.critical || 'unknown',
-                recoveredCases: countryData.cases.recovered || 'unknown',
-                totalCases: countryData.cases.total || 'unknown'
-            }
-            
-            createHtml(country)
-
-            makeAGraph([
-                {x: "Population Without Covid Diagnosis", value: difference(parseInt(country.population), parseInt(country.totalCases))},
-                {x: "Population With Covid Diagnosis", value: parseInt(country.totalCases)}
-              ], `Percentage of ${country.country}'s Population With Positive Covid Diagnosis`, document.getElementById('one'))
-
-            makeAGraph([
-                {x: "Total Deaths", value: parseInt(country.deaths)},
-                {x: "Active Cases", value: parseInt(country.activeCases)},
-                {x: "Recovered Cases", value: parseInt(country.recoveredCases)}
-              ], `Breakdown of Total Positive Covid Cases in ${country.country}`, document.getElementById('two'))
-        })
-}
 
 let createHtml = (country) => {
     let titleLabel = `
@@ -93,14 +55,6 @@ let makeAGraph = (data, title, container) => {
 
 let difference = (x, y) => x-y
 
-let countryList = (data) => {
-    let options = data.response.map(item => `
-        <option value='${item}'>${item}</option>
-    `).join('')
-    countries.innerHTML = options
-    countryStats()
-}
-
 //alerts end user to server error
 let statusCheck = (response) => {
     if(response.ok) return Promise.resolve(response)
@@ -109,14 +63,139 @@ let statusCheck = (response) => {
         error.innerHTML = `We're sorry, there was an error processing this request. Please verify your api key, refresh the page and try again.`
         console.log(response.statusText)
     }
-} 
+}     
 
 let getJson = (url, headers) => fetch(url, headers)
     .then(statusCheck)
     .then(res => res.json())
 
-let getCountries = getJson(countriesUrl, apiheaders) 
-                    .then(data => countryList(data))                     
+let countryList = (data) => {
+    let options = data.response.map(item => `
+        <option value='${item}'>${item}</option>
+    `).join('')
+    countries.innerHTML = options
+    countryStats()
+}
 
-getCountries  
+let getCountries = getJson(countriesUrl, apiheaders) 
+                    .then(data => countryList(data))     
+
+let countryStats = () => {
+    const selectedCountry = countries.value 
+    
+    getJson(`${statisticsUrl}?country=${selectedCountry}`, apiheaders)
+        .then(data => {
+            const countryData = data.response[0]
+
+            country = {
+                continent: countryData.continent || 'unknown',
+                continent: countryData.continent || 'unknown',
+                country: countryData.country || 'unknown',
+                day: countryData.day || 'unknown',
+                deaths: countryData.deaths.total || 'unknown',
+                population: countryData.population || 'unknown',
+                tests: countryData.tests.total || 'unknown',
+                newCases: countryData.cases.new || 'unknown',
+                activeCases: countryData.cases.active || 'unknown',
+                criticalCases: countryData.cases.critical || 'unknown',
+                recoveredCases: countryData.cases.recovered || 'unknown',
+                totalCases: countryData.cases.total || 'unknown'
+            }
+            
+            createHtml(country)
+
+            makeAGraph([
+                {x: "Population Without Covid Diagnosis", value: difference(parseInt(country.population), parseInt(country.totalCases))},
+                {x: "Population With Covid Diagnosis", value: parseInt(country.totalCases)}
+              ], `Percentage of ${country.country}'s Population With Positive Covid Diagnosis`, document.getElementById('one'))
+
+            makeAGraph([
+                {x: "Total Deaths", value: parseInt(country.deaths)},
+                {x: "Active Cases", value: parseInt(country.activeCases)},
+                {x: "Recovered Cases", value: parseInt(country.recoveredCases)}
+              ], `Breakdown of Total Positive Covid Cases in ${country.country}`, document.getElementById('two'))
+        })
+}
+
+const countryCodes = fetch('https://cdn.anychart.com/samples/maps-choropleth/world-governments-map/data.json')
+                   .then(res => res.json())
+          
+//dataset for map
+const worldStats = getJson(statisticsUrl, apiheaders)
+                .then(data => data.response)
+                .then(async res => {
+                    const countryCode = await countryCodes
+                    let country
+                    let obj
+                    let arr = [
+                        {
+                            "id":"Somaliland",
+                            "value": 0
+                        },
+                        {
+                            "id":"TM",
+                            "value": 0
+                        },
+                        {
+                            "id":"KP",
+                            "value": 0
+                        }
+                    ]
+
+                    for (var r of res) {
+                        country = countryCode.find(item => item.name === r.country)
+                        if(!country) 
+                            country = countryIds.find(item => item.country === r.country)                        
+
+                        if(country) {
+                            obj = {
+                                "id":country.id,
+                                "value": r.cases.total,
+                                "country":r.country,
+                                "deaths": r.deaths.total,
+                                "population": r.population
+                            }
+                            
+                            arr.push(obj)
+                        }
+                    }
+                    
+                    return arr
+                })
+
+anychart.onDocumentReady(async function () {
+    const data = await worldStats
+    
+    const map = anychart.map();
+    map.geoData(anychart.maps.world);
+
+    let series = map.choropleth(data);
+    series.colorScale(anychart.scales.linearColor('#81d4fa', '#014377'));
+    series.hovered().fill('#302E2F');
+
+    map.tooltip()
+        .useHtml(true)
+        // .titleFormat(function() {
+        //     return this.getData('country')
+        // })
+        .format(function () {
+            const dataDisplay = `
+                                Positive Cases: ${this.value}  <br/> 
+                                Deaths: ${this.getData('deaths')}  <br/> 
+                                Total Population: ${this.getData('population')} 
+                                `
+            const noData = `<span style="background-color: grey">
+                           Unknown Covid Data</span>
+                           `                                
+        return this.value === 0 ? noData : dataDisplay
+    })
+
+    // set the container
+    map.container('world');
+    map.draw();
+  });         
+
+//do the work  
+//getCountries
 countries.addEventListener('change', countryStats)            
+
